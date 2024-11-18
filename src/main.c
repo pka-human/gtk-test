0d134704cc234572ee2ef115ef47605912570555
pka-human <o0o0.pe4Ka.0o0o@gmail.com>
2024-11-19 01:48:43 +0500

    add interface

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-int main() {
-    printf("Hello, World!\n");
+void on_button_clicked(GtkWidget *widget, gpointer data) {
+    g_print("Button clicked!\n");
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Simple GTK Interface");
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+
+    GtkWidget *button = gtk_button_new_with_label("Click me");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+
+    gtk_container_add(GTK_CONTAINER(window), button);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
     return 0;
 }
