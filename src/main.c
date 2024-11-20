aacdb20c853ffc9f11392e9668b9a6b74fdd6841
votbotcot <ogures.ogursov@gmail.com>
2024-11-20 13:57:46 +0200

    add new button

--- src/main.c
+++ src/main.c
@@ -1,24 +1,47 @@
 #include <gtk/gtk.h>
 
-void on_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("Button clicked!\n");
+void pickme_clicked(GtkWidget *button, gpointer user_data) {
+    g_print("pickme\n");
+}
+
+void sex_clicked(GtkWidget *button, gpointer user_data) {
+    g_print("SEX SEX SEX!\n");
 }
 
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Simple GTK Interface");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    // windows fuck you
+    GtkWidget *main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(main_window), "Simple GTK Interface");
+    gtk_window_set_default_size(GTK_WINDOW(main_window), 300, 200);
 
-    GtkWidget *button = gtk_button_new_with_label("Click me");
-    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    // грид
+    GtkWidget *grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(main_window), grid);
 
-    gtk_container_add(GTK_CONTAINER(window), button);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    // пикми
+    GtkWidget *pickme_button = gtk_button_new_with_label("Click me");
+    g_signal_connect(pickme_button, "clicked", G_CALLBACK(pickme_clicked), NULL);
 
-    gtk_widget_show_all(window);
+    // сэкс
+    GtkWidget *s_button = gtk_button_new_with_label("Sex");
+    g_signal_connect(s_button, "clicked", G_CALLBACK(sex_clicked), NULL);
+
+    // сайз
+    gtk_widget_set_size_request(pickme_button, 120, 50);
+    gtk_widget_set_size_request(s_button, 120, 50);
+
+    // кнопки в грид
+    gtk_grid_attach(GTK_GRID(grid), pickme_button, 0, 0, 1, 1); 
+    gtk_grid_attach(GTK_GRID(grid), s_button, 1, 0, 1, 1);
+  
+    g_signal_connect(main_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(main_window);
+
     gtk_main();
 
     return 0;
 }
+
